b39bb26337c8dbe3c9d6b75773adf7b3316620ba
parvajio <mdparvajmosharof17@gmail.com>
2025-09-20 17:29:35 +0600

    create func for post api

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,26 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { DatabaseService } from 'src/database/database.service';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor (private db: DatabaseService){}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    try {
+      const category = await this.db.category.create({
+        data: createCategoryDto,
+        include: {
+          _count:{
+            select:{
+              products: true
+            }
+          }
+        }
+      })
+
+      return {
+        ...category,
+        productCount: category._count.products
+      }
+    } catch (error) {
+      if (error.code === 'P2002') {
+        throw new ConflictException('Category with this name already exists');
+      }
+      throw error;
+    }
   }
 
-  findAll() {
+  async findAll() {
     return `This action returns all categories`;
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     return `This action returns a #${id} category`;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
     return `This action updates a #${id} category`;
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     return `This action removes a #${id} category`;
   }
 }
