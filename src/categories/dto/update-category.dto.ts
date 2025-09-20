4f614363c586f7e354d458f25032d4fb19aba214
parvajio <mdparvajmosharof17@gmail.com>
2025-09-20 17:08:44 +0600

    update category dto

--- src/categories/dto/update-category.dto.ts
+++ src/categories/dto/update-category.dto.ts
@@ -1,4 +1,18 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCategoryDto } from './create-category.dto';
+import { ApiProperty } from "@nestjs/swagger";
+import { IsOptional, IsString, MinLength } from "class-validator";
 
-export class UpdateCategoryDto extends PartialType(CreateCategoryDto) {}
+export class UpdateCategoryDto {
+  @ApiProperty({ example: 'Electronics', required: false })
+  @IsOptional()
+  @IsString()
+  @MinLength(2)
+  name?: string;
+
+  @ApiProperty({
+    example: 'Electronic devices and accessories',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  description?: string;
+}
