e3e87ba0e9cc5d262356e18456bb7bb323494acc
parvajio <mdparvajmosharof17@gmail.com>
2025-09-20 15:51:01 +0600

    database module and service created

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatebaseModule } from './datebase/datebase.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [DatebaseModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
