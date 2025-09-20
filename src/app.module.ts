1b5be0acffa799a6c8f940b8a3228ba89be6b772
parvajio <mdparvajmosharof17@gmail.com>
2025-09-20 21:59:20 +0600

    auth resource created by nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ProductsModule } from './products/products.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, CategoriesModule, ProductsModule],
+  imports: [DatabaseModule, CategoriesModule, ProductsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
