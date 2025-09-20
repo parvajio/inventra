79254e129eab2fe59b1baf4ab6200dd341a00d0f
parvajio <mdparvajmosharof17@gmail.com>
2025-09-20 18:41:48 +0600

    create product dto

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,40 @@
-export class CreateProductDto {}
+import { ApiProperty } from "@nestjs/swagger";
+import { Type } from "class-transformer";
+import { IsNumber, IsOptional, IsPositive, IsString, IsUUID, MinLength } from "class-validator";
+
+export class CreateProductDto {
+  @ApiProperty({ example: 'iPhone 15 Pro' })
+  @IsString()
+  @MinLength(2)
+  name: string;
+
+  @ApiProperty({
+    example: 'Latest iPhone with advanced features',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @ApiProperty({ example: 999.99 })
+  @IsNumber()
+  @IsPositive()
+  @Type(() => Number)
+  price: number;
+
+  @ApiProperty({ example: 50 })
+  @IsNumber()
+  @IsPositive()
+  @Type(() => Number)
+  stock: number;
+
+  @ApiProperty({ example: 'https://example.com/image.jpg', required: false })
+  @IsOptional()
+  @IsString()
+  imageUrl?: string;
+
+  @ApiProperty({ example: 'clx1234567890abcdef' })
+  @IsString()
+  @IsUUID()
+  categoryId: string;
+}
