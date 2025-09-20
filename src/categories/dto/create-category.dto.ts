39945120a0daa00946c8d39e4ccb079d26ee1216
parvajio <mdparvajmosharof17@gmail.com>
2025-09-20 17:06:37 +0600

    create category dto

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,1 +1,17 @@
-export class CreateCategoryDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsOptional, IsString, MinLength } from 'class-validator';
+
+export class CreateCategoryDto {
+  @ApiProperty({ example: 'Electronics' })
+  @IsString()
+  @MinLength(2)
+  name: string;
+
+  @ApiProperty({
+    example: 'Electronic devices and accessories',
+    required: false,
+  })
+  @IsOptional()
+  @IsString()
+  description?: string;
+}
