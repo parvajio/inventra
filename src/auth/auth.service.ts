7418f06fad518b2dc806894491cc61b48918ada9
parvajio <mdparvajmosharof17@gmail.com>
2025-09-21 11:23:35 +0600

    login method with user validation and pass validation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,6 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
-import { RegisterDto } from './dto/auth.dto';
+import { LoginDto, RegisterDto } from './dto/auth.dto';
 import * as bcrypt from 'bcrypt'
 
 @Injectable()
@@ -37,6 +37,25 @@ export class AuthService {
     })
 
     return user
+  }
+
+  async login(loginDto: LoginDto){
+    const {email, password} = loginDto;
+
+    const user = await this.db.user.findUnique({
+      where: {email}
+    })
+
+    if(!user){
+      throw new UnauthorizedException("Inavalid user info")
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if(!isPasswordValid){
+      throw new UnauthorizedException("navalid user info")
+    }
+
+    
   }
 
   async validateUser(userId: string){
