a81f1fb97d4cb846d0a4f037085b27f0cc16fb8f
parvajio <mdparvajmosharof17@gmail.com>
2025-09-21 11:31:26 +0600

    basic jwtmodule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports:[
+    JwtModule.register({
+      secret: "abc1234",
+      signOptions: {expiresIn: '1h'}
+    })
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
