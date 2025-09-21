664b4030ddca52c282b7f6b2120ace13bddef34c
parvajio <mdparvajmosharof17@gmail.com>
2025-09-21 11:39:35 +0600

    token generate and return with user info

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,73 +1,101 @@
-import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 import { LoginDto, RegisterDto } from './dto/auth.dto';
-import * as bcrypt from 'bcrypt'
+import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor (private db: DatabaseService) {}
+  constructor(
+    private db: DatabaseService,
+    private jwtService: JwtService,
+  ) {}
 
-  async register(registerDto: RegisterDto){
-    const {email, username, password} = registerDto;
+  async register(registerDto: RegisterDto) {
+    const { email, username, password } = registerDto;
 
     const existingUser = await this.db.user.findFirst({
       where: {
-        OR:[{email}, {username}]
+        OR: [{ email }, { username }],
       },
-    })
+    });
 
-    if(existingUser){
-      throw new ConflictException('User with this email or username already exists')
+    if (existingUser) {
+      throw new ConflictException(
+        'User with this email or username already exists',
+      );
     }
 
-    const hashedPassword = await bcrypt.hash(password, 10)
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await this.db.user.create({
-      data:{
+      data: {
         email,
-        username, 
-        password: hashedPassword
+        username,
+        password: hashedPassword,
       },
-      select:{
-        id: true,
-        email:true,
-        username: true,
-        createdAt: true
-      }
-    })
-
-    return user
-  }
-
-  async login(loginDto: LoginDto){
-    const {email, password} = loginDto;
-
-    const user = await this.db.user.findUnique({
-      where: {email}
-    })
-
-    if(!user){
-      throw new UnauthorizedException("Inavalid user info")
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if(!isPasswordValid){
-      throw new UnauthorizedException("navalid user info")
-    }
-
-    
-  }
-
-  async validateUser(userId: string){
-    const user = await this.db.user.findUnique({
-      where:{id: userId},
-      select:{
+      select: {
         id: true,
         email: true,
         username: true,
-        createdAt: true
-      }
-    })
+        createdAt: true,
+      },
+    });
+
+    return user;
+  }
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+
+    // user from db
+    const user = await this.db.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Inavalid user info');
+    }
+
+    // password check
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('navalid user info');
+    }
+
+    // jwt token
+    const payload = {
+      sub: user.id,
+      email: user.email,
+      username: user.username,
+    };
+    const token = this.jwtService.sign(payload);
+
+    return {
+      user: {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+        createdAt: user.createdAt,
+      },
+      token,
+    };
+  }
+
+  async validateUser(userId: string) {
+    const user = await this.db.user.findUnique({
+      where: { id: userId },
+      select: {
+        id: true,
+        email: true,
+        username: true,
+        createdAt: true,
+      },
+    });
 
     return user;
   }
