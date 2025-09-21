9691a56c0d10b11c09e04971b6935387bd1fe1b5
parvajio <mdparvajmosharof17@gmail.com>
2025-09-21 11:48:18 +0600

    imported passport module in auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports:[
+    PassportModule,
     JwtModule.register({
       secret: "abc1234",
       signOptions: {expiresIn: '1h'}
